515ab76951aac1b4a0096dfff357bc9c58caa568
Alexander Sokolov <ownger@gmail.com>
2020-07-08 00:24:03 +0300

    Обновил экспор функций из файла index.ts

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,2 +1,5 @@
 export * from "./sortOddNumbers/sortOddNumbers";
 export * from "./countLettersInString/countLettersInString";
+export * from "./randomHexNumber/randomHexNumber";
+export * from "./sumNumbersFromRandomArray/sumNumbersFromRandomArray";
+export * from "./sumAllDigitsInNumber/sumAllDigitsInNumber";
